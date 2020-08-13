4dca3175455193d6738fbc5a38fc62059efff151
Silvio Kennecke <silvio@silvio-kennecke.de>
2020-08-13 23:25:03 +0200

    add AbstractApi

--- src/VereinsfliegerApi/Api/AbstractApi.php
+++ src/VereinsfliegerApi/Api/AbstractApi.php
@@ -4,6 +4,29 @@
 namespace Diginize\WpVereinsflieger\VereinsfliegerApi\Api;
 
 
+use GuzzleHttp\Client;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
 class AbstractApi {
+
+	/** @var Client */
+	protected $gruzzle;
+
+	/** @var Serializer */
+	protected $serializer;
+
+	public function __construct() {
+		$this->gruzzle = new Client();
+		$this->serializer = new Serializer([new ObjectNormalizer()], [new JsonEncoder()]);
+	}
+
+	protected function deserializeResponse(string $responseBody, $targetType) {
+		$this->serializer->deserialize($responseBody, $targetType, 'json', [
+			AbstractNormalizer::ALLOW_EXTRA_ATTRIBUTES => false
+		]);
+	}
 
 }
