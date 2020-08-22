b48f056a75bcfb68c12892c484650e6fe213a8f9
Silvio Kennecke <skennecke@diginize.de>
2020-08-22 14:56:21 +0200

    update version

--- wp-vereinsflieger.php
+++ wp-vereinsflieger.php
@@ -6,7 +6,7 @@
  * Donate link: https://paypal.me/diginize
  * Tags: sso, vereinsflieger, luftsport
  * Description: Allows users to login into wordpress using their Vereinsflieger login.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Diginize
  * Author URI: https://www.diginize.de
  * License: GPLv3
